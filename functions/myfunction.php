3bb6dcb0b9d11bf36091ba5fee0eacaa04548bfb
SaraMaktabi <Saramaktabi99@gmail.com>
2023-08-01 19:19:58 +0100

    edit

--- functions/myfunction.php
+++ functions/myfunction.php
@@ -15,7 +15,7 @@ function getAll($table) {
     return $result;
 }
 
-/*function getById($table, $id) {
+function getById($table, $id) {
     global $conn;
     $query = "SELECT * FROM $table WHERE id='$id'";
     $result = mysqli_query($conn, $query);
@@ -26,26 +26,7 @@ function getAll($table) {
     }
 
     return $result;
-}*/
-
-function getById($table, $id) {
-    global $conn;
-
-    // Use prepared statement to prevent SQL injection
-    $query = "SELECT * FROM $table WHERE id = ?";
-    $stmt = mysqli_prepare($conn, $query);
-    mysqli_stmt_bind_param($stmt, "i", $id);
-    mysqli_stmt_execute($stmt);
-    $result = mysqli_stmt_get_result($stmt);
-
-    if (!$result) {
-        // Query execution failed, handle the error
-        die("Error: " . mysqli_error($conn));
-    }
-
-    return $result;
 }
-
 
 function redirect($url, $message) {
     $_SESSION['message'] = $message;
